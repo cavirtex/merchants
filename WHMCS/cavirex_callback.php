e45e94d0e70f8837c3c760281cce41b7d03a9faa
Yasyf Mohamedali <yasyfm@gmail.com>
2013-11-29 01:44:56 -0500

    added WHMCS

---
+++ WHMCS/cavirex_callback.php
@@ -0,0 +1,41 @@
+<?php
+
+# Required File Includes
+include("../../../dbconnect.php");
+include("../../../includes/functions.php");
+include("../../../includes/gatewayfunctions.php");
+include("../../../includes/invoicefunctions.php");
+
+$gatewaymodule = "cavirtex"; # Enter your gateway module name here replacing template
+
+$GATEWAY = getGatewayVariables($gatewaymodule);
+if (!$GATEWAY["type"]) die("Module Not Activated"); # Checks gateway module is active before accepting callback
+
+$key = $GATEWAY['merchant_key'];
+$sk = $GATEWAY['merchant_sk'];
+
+# Get Returned Variables - Adjust for Post Variable Names from your Gateway's Documentation
+$ipn = json_decode(file_get_contents('php://input'));
+$btc_received = $ipn["btc_received"];
+$order_key = $ipn["order_key"];
+$invoice_number = $ipn["invoice_number"];
+
+
+$url = 'https://www.cavirtex.com/merchant_confirm_ipn';
+$postfields = array( 'secret_key' => $sk, 'order_key' => $order_key,'btc_received' => $btc_received,'invoice_number' => $invoice_number );
+$response = json_decode(curlCall($url,$postfields,$options));
+
+$invoice_number = checkCbInvoiceID($invoice_number,$GATEWAY['name']); # Checks invoice ID is a valid invoice number or ends processing
+
+checkCbTransID($invoice_number); # Checks transaction number isn't already in the database and ends processing if it does
+
+if ($response["status"] != "error") {
+    # Successful
+    addInvoicePayment($invoice_number,$order_key,$amount,$fee,$gatewaymodule); # Apply Payment to Invoice: invoiceid, transactionid, amount paid, fees, modulename
+	logTransaction($GATEWAY["name"],$_POST,"Successful"); # Save to Gateway Log: name, data array, status
+} else {
+	# Unsuccessful
+    logTransaction($GATEWAY["name"],$_POST,"Unsuccessful"); # Save to Gateway Log: name, data array, status
+}
+
+?>
---
+++ WHMCS/cavirtex.php
@@ -0,0 +1,67 @@
+<?php
+
+function cavirtex_config() {
+    $configarray = array(
+     "FriendlyName" => array("Type" => "System", "Value"=>"CaVirtex"),
+     "merchant_key" => array("FriendlyName" => "Merchant Key", "Type" => "text", "Size" => "40", ),
+     "merchant_sk" => array("FriendlyName" => "Merchant Secret Key", "Type" => "text", "Size" => "40", ),
+     "shipping_required" => array("FriendlyName" => "Shipping Required", "Type" => "yesno", ),
+    );
+	return $configarray;
+}
+
+function cavirtex_link($params) {
+
+	# Gateway Specific Variables
+	$key = $params['merchant_key'];
+	$sk = $params['merchant_sk'];
+	$ship = $params['shipping_required'];
+
+	# Invoice Variables
+	$invoiceid = $params['invoiceid'];
+	$description = $params["description"];
+    $amount = $params['amount']; # Format: ##.##
+    $currency = $params['currency']; # Currency Code
+
+	# Client Variables
+	$firstname = $params['clientdetails']['firstname'];
+	$lastname = $params['clientdetails']['lastname'];
+	$email = $params['clientdetails']['email'];
+	$address1 = $params['clientdetails']['address1'];
+	$address2 = $params['clientdetails']['address2'];
+	$city = $params['clientdetails']['city'];
+	$state = $params['clientdetails']['state'];
+	$postcode = $params['clientdetails']['postcode'];
+	$country = $params['clientdetails']['country'];
+	$phone = $params['clientdetails']['phonenumber'];
+
+	# System Variables
+	$companyname = $params['companyname'];
+	$systemurl = $params['systemurl'];
+	$currency = $params['currency'];
+
+	# Enter your code submit to the gateway...
+
+	$code = '<form method="https://www.cavirtex.com/merchant_purchase/'.$key.'">
+<input type="hidden" name="name" value="Invoice '.$invoiceid.' ('.$description.')" />
+<input type="hidden" name="code" value="'.$invoiceid.'" />
+<input type="hidden" name="price" value="'.$amount.'" />
+<input type="hidden" name="shipping_required" value="'.$ship.'" />
+<input type="hidden" name="amount" value="'.$amount.'" />
+<input type="hidden" name="email" value="'.$email.'" />
+<input type="hidden" name="customer_name" value="'.$firstname.' '.$lastname.'" />
+<input type="hidden" name="address" value="'.$address1.'" />
+<input type="hidden" name="address2" value="'.$address2.'" />
+<input type="hidden" name="city" value="'.$city.'" />
+<input type="hidden" name="province" value="'.$state.'" />
+<input type="hidden" name="postal" value="'.$postcode.'" />
+<input type="hidden" name="country" value="'.$country.'" />
+<input type="submit" value="Pay Now" />
+</form>';
+
+	return $code;
+}
+
+
+
+?>
